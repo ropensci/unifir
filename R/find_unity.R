d9cec60731d5a3728d397d921cf76b1ea8813c6b
Mike Mahoney <mike.mahoney.218@gmail.com>
2021-06-04 11:12:42 -0400

    Check a few standard paths

--- R/find_unity.R
+++ R/find_unity.R
@@ -8,9 +8,33 @@
 #'
 #' @export
 find_unity <- function() {
+
   unity <- Sys.getenv("unifir_unity_path")
 
-  if (unity == '') unity <- options("unifir_unity_path")
+  if (unity == '') unity <- options("unifir_unity_path")[[1]]
+
+  # Check OS standard locations...
+  if (is.null(unity) || !file.exists(unity)) {
+    sysname <- tolower(Sys.info()[["sysname"]])
+    if ("windows" == sysname) {
+      if (dir.exists("C:\\Program Files\\Unity\\Hub\\Editor")) {
+        unity <- tail(
+          list.files("C:\\Program Files\\Unity\\Hub\\Editor",
+                     full.names = TRUE),
+          1)
+        unity <- paste0(unity, "\\Editor\\Unity.exe")
+      }
+    }
+
+    if ("linux" == sysname) {
+      if (dir.exists("~/Unity/Hub/Editor")) {
+        unity <- tail(list.files("~/Unity/Hub/Editor", full.names = TRUE), 1)
+        unity <- paste0(unity, "/Editor/Unity")
+      }
+    }
+
+    # TODO: MacOS goes here; I don't have a system to test with
+  }
 
   if (is.null(unity) || !file.exists(unity)) {
     stop("Couldn't find Unity executable at provided path. \n",
